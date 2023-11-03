faae492f5da8a808f276d02a4d5207fda54ceba8
Igor Lunyakin <lunyaigor484@gmail.com>
2023-11-03 13:27:38 +0200

    add replace to navigate

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -7,35 +7,6 @@ const initilaValue = {
   error: null,
 };
 
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState: initilaValue,
-//   extraReducers: {
-//     [fetchContacts.pending]: handlePending,
-//     [fetchContacts.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items = action.payload;
-//     },
-//     [fetchContacts.rejected]: handleRejected,
-//     [addContact.pending]: handlePending,
-//     [addContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items.push(action.payload);
-//     },
-//     [addContact.rejected]: handleRejected,
-//     [deleteContact.pending]: handlePending,
-//     [deleteContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       const index = state.items.findIndex(item => item.id === action.payload);
-//       state.items.splice(index, 1);
-//     },
-//     [deleteContact.rejected]: handleRejected,
-//   },
-// });
-
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: initilaValue,
